ef862e9d939fb51a926539dbf5146e433fde2ffe
Bojan Losic <bojan.onwork@gmail.com>
2021-06-08 15:10:11 +0200

    nfc test

--- src/screens/App/Home/HomeScreen.js
+++ src/screens/App/Home/HomeScreen.js
@@ -2,9 +2,47 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { LOADING_API, SHOW_CAMERA } from "../../../redux/types/actionTypes";
 import HomeView from "./HomeView";
+import NfcManager, { NfcEvents } from "react-native-nfc-manager";
+
+// Pre-step, call this before any NFC operations
+async function initNfc() {
+  await NfcManager.start();
+}
+
+function readNdef() {
+  const cleanUp = () => {
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+    NfcManager.setEventListener(NfcEvents.SessionClosed, null);
+  };
+
+  return new Promise((resolve) => {
+    let tagFound = null;
+
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
+      tagFound = tag;
+      resolve(tagFound);
+      NfcManager.setAlertMessageIOS("NDEF tag found");
+      NfcManager.unregisterTagEvent().catch(() => 0);
+    });
+
+    NfcManager.setEventListener(NfcEvents.SessionClosed, () => {
+      cleanUp();
+      if (!tagFound) {
+        resolve();
+      }
+    });
+
+    NfcManager.registerTagEvent();
+  });
+}
 
 const Home = () => {
   const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    initNfc();
+    readNdef();
+  }, []);
 
   const smaraj = () => {
     dispatch({ type: LOADING_API, payload: true });
