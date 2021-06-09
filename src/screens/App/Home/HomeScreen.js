afbe6a5360d1eadfda1a67b31efd741368a35af9
Bojan Losic <bojan.onwork@gmail.com>
2021-06-09 10:13:52 +0200

    draft

--- src/screens/App/Home/HomeScreen.js
+++ src/screens/App/Home/HomeScreen.js
@@ -2,9 +2,11 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { LOADING_API, SHOW_CAMERA } from "../../../redux/types/actionTypes";
 import HomeView from "./HomeView";
+import { Audio } from "expo-av";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const [sound, setSound] = React.useState();
 
   const smaraj = () => {
     dispatch({ type: LOADING_API, payload: true });
@@ -15,7 +17,32 @@ const Home = () => {
     }, 3000);
   };
 
-  return <HomeView dispatch={dispatch} SHOW_CAMERA={SHOW_CAMERA} smaraj={smaraj} />;
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(require("../../../../assets/test.mp3"));
+    await sound.setIsMutedAsync(false);
+    await Audio.setAudioModeAsync({ staysActiveInBackground: true });
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  async function stopSound() {
+    console.log("Stop Sound");
+    await sound.stopAsync();
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  return <HomeView dispatch={dispatch} SHOW_CAMERA={SHOW_CAMERA} smaraj={smaraj} playSound={playSound} stopSound={stopSound} />;
 };
 
 export default Home;
--- src/screens/App/Home/HomeView.js
+++ src/screens/App/Home/HomeView.js
@@ -3,12 +3,14 @@ import { Button, Text, View } from "react-native";
 import { mainStyle as style } from "@styles";
 import styles from "./Styles";
 
-export default ({ dispatch, smaraj, SHOW_CAMERA }) => {
+export default ({ dispatch, smaraj, SHOW_CAMERA, playSound, stopSound }) => {
   return (
     <View style={style.container}>
       <Text>Home</Text>
       <Button title="Show camera" onPress={() => dispatch({ type: SHOW_CAMERA, payload: true })} />
       <Button title="smaraj" onPress={smaraj} />
+      <Button title="Play Sound" onPress={playSound} />
+      <Button title="Stop Sound" onPress={stopSound} />
     </View>
   );
 };
